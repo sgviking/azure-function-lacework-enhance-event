e63be947e595e51304f573231bd4dd8fe68e888b
Dan Daggett <dan.daggett@lacework.net>
2023-12-22 11:07:05 -0800

    Fix bugs in get_csp_info() function.

--- lacework-ingest/__init__.py
+++ lacework-ingest/__init__.py
@@ -8,42 +8,42 @@ from laceworksdk import LaceworkClient
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     def get_csp_info(alert):
-        entityMap = alert.get('entityMap')
+        entityMap = alert.get('entitymap')
         # Azure alerts have information in entityMap->AzureIdentity
         if entityMap.get('AzureIdentity', False):
-            alert['azure_principal'] = entityMap.AzureIdentity[0].get('KEY', {}).get('principal_id')
-            alert['azure_subscription_id'] = entityMap.AzureIdentity[0].get('PROPS', {}).get('subscription_list', [])[0].get('subscription_id')
-            alert['azure_tenant_id'] = entityMap.AzureIdentity[0].get('PROPS', {}).get('subscription_list', [])[0].get('tenant_id')
+            alert['azure_principal'] = entityMap['AzureIdentity'][0].get('KEY', {}).get('principal_id')
+            alert['azure_subscription_id'] = entityMap['AzureIdentity'][0].get('PROPS', {}).get('subscription_list', [])[0].get('subscription_id')
+            alert['azure_tenant_id'] = entityMap['AzureIdentity'][0].get('PROPS', {}).get('subscription_list', [])[0].get('tenant_id')
             return alert
 
         # GCP alerts have information in entityMap->GcpIdentity and entityMap->Project
         if entityMap.get('GcpIdentity', False) and entityMap.get('Project', False):
-            alert['gcp_principal_email'] = entityMap.GcpIdentity[0].get('KEY', {}).get('principal_email')
-            alert['gcp_organization_id'] = entityMap.Project[0].get('KEY', {}).get('organization_id')
-            alert['gcp_organization_name'] = entityMap.Project[0].get('KEY', {}).get('organization_name')
-            alert['gcp_project_id'] = entityMap.Project[0].get('KEY', {}).get('project_id')
+            alert['gcp_principal_email'] = entityMap['GcpIdentity'][0].get('KEY', {}).get('principal_email')
+            alert['gcp_organization_id'] = entityMap['Project'][0].get('KEY', {}).get('organization_id')
+            alert['gcp_organization_name'] = entityMap['Project'][0].get('KEY', {}).get('organization_name')
+            alert['gcp_project_id'] = entityMap['Project'][0].get('KEY', {}).get('project_id')
             return alert
 
         # AWS compliance alerts have information in entityMap->RecId
         if entityMap.get('RecId', False):
-            alert['aws_account_id'] = entityMap.RecId[0].get('PROPS', {}).get('account_id')
-            alert['aws_account_alias'] = entityMap.RecId[0].get('PROPS', {}).get('account_alias')
+            alert['aws_account_id'] = entityMap['RecId'][0].get('PROPS', {}).get('account_id')
+            alert['aws_account_alias'] = entityMap['RecId'][0].get('PROPS', {}).get('account_alias')
             return alert
 
         # AWS CloudTrail alerts have information in entityMap->CT_User
         if entityMap.get('CT_User', False):
-            alert['aws_account_id'] = entityMap.CT_User[0].get('KEY', {}).get('account')
-            alert['aws_principal_id'] = entityMap.CT_User[0].get('KEY', {}).get('principalId')
-            alert['aws_username'] = entityMap.CT_User[0].get('KEY', {}).get('username')
+            alert['aws_account_id'] = entityMap['CT_User'][0].get('KEY', {}).get('account')
+            alert['aws_principal_id'] = entityMap['CT_User'][0].get('KEY', {}).get('principalId')
+            alert['aws_username'] = entityMap['CT_User'][0].get('KEY', {}).get('username')
             return alert
 
         # AWS host alerts have information in entityMap->Machine
         if entityMap.get('Machine', False):
-            alert['aws_account_id'] = entityMap.Machine[1].get('PROPS', {}).get('tags', {}).get('Account'))
-            alert['ic:site_id'] = entityMap.Machine[1].get('PROPS', {}).get('tags', {}).get('ic:siteid'))
-            alert['ic:vcs_account_id'] = entityMap.Machine[1].get('PROPS', {}).get('tags', {}).get('ic:vcs-account-id')
-            alert['site_id'] = entityMap.Machine[1].get('PROPS', {}).get('tags', {}).get('siteid'))
-            alert['vcs_account_id'] = entityMap.Machine[1].get('PROPS', {}).get('tags', {}).get('vcs-account-id')
+            alert['aws_account_id'] = entityMap['Machine'][0].get('PROPS', {}).get('tags', {}).get('Account')
+            alert['ic:site_id'] = entityMap['Machine'][0].get('PROPS', {}).get('tags', {}).get('ic:siteid')
+            alert['ic:vcs_account_id'] = entityMap['Machine'][0].get('PROPS', {}).get('tags', {}).get('ic:vcs-account-id')
+            alert['site_id'] = entityMap['Machine'][0].get('PROPS', {}).get('tags', {}).get('siteid')
+            alert['vcs_account_id'] = entityMap['Machine'][0].get('PROPS', {}).get('tags', {}).get('vcs-account-id')
             return alert
 
         return alert
