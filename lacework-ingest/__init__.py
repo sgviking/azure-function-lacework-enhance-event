8ed0d892cbce5b5f3dda1853303b73bf7aa660d5
Dan Daggett <dan.daggett@lacework.net>
2023-12-22 09:19:16 -0800

    Add AWS, GCP, and Azure information to alerts.

--- lacework-ingest/__init__.py
+++ lacework-ingest/__init__.py
@@ -7,6 +7,46 @@ from laceworksdk import LaceworkClient
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
+    def get_csp_info(alert):
+        entityMap = alert.get('entityMap')
+        # Azure alerts have information in entityMap->AzureIdentity
+        if entityMap.get('AzureIdentity', False):
+            alert['azure_principal'] = entityMap.AzureIdentity[0].get('KEY', {}).get('principal_id')
+            alert['azure_subscription_id'] = entityMap.AzureIdentity[0].get('PROPS', {}).get('subscription_list', [])[0].get('subscription_id')
+            alert['azure_tenant_id'] = entityMap.AzureIdentity[0].get('PROPS', {}).get('subscription_list', [])[0].get('tenant_id')
+            return alert
+
+        # GCP alerts have information in entityMap->GcpIdentity and entityMap->Project
+        if entityMap.get('GcpIdentity', False) and entityMap.get('Project', False):
+            alert['gcp_principal_email'] = entityMap.GcpIdentity[0].get('KEY', {}).get('principal_email')
+            alert['gcp_organization_id'] = entityMap.Project[0].get('KEY', {}).get('organization_id')
+            alert['gcp_organization_name'] = entityMap.Project[0].get('KEY', {}).get('organization_name')
+            alert['gcp_project_id'] = entityMap.Project[0].get('KEY', {}).get('project_id')
+            return alert
+
+        # AWS compliance alerts have information in entityMap->RecId
+        if entityMap.get('RecId', False):
+            alert['aws_account_id'] = entityMap.RecId[0].get('PROPS', {}).get('account_id')
+            alert['aws_account_alias'] = entityMap.RecId[0].get('PROPS', {}).get('account_alias')
+            return alert
+
+        # AWS CloudTrail alerts have information in entityMap->CT_User
+        if entityMap.get('CT_User', False):
+            alert['aws_account_id'] = entityMap.CT_User[0].get('KEY', {}).get('account')
+            alert['aws_principal_id'] = entityMap.CT_User[0].get('KEY', {}).get('principalId')
+            alert['aws_username'] = entityMap.CT_User[0].get('KEY', {}).get('username')
+            return alert
+
+        # AWS host alerts have information in entityMap->Machine
+        if entityMap.get('Machine', False):
+            alert['aws_account_id'] = entityMap.Machine[1].get('PROPS', {}).get('tags', {}).get('Account'))
+            alert['ic:site_id'] = entityMap.Machine[1].get('PROPS', {}).get('tags', {}).get('ic:siteid'))
+            alert['ic:vcs_account_id'] = entityMap.Machine[1].get('PROPS', {}).get('tags', {}).get('ic:vcs-account-id')
+            alert['site_id'] = entityMap.Machine[1].get('PROPS', {}).get('tags', {}).get('siteid'))
+            alert['vcs_account_id'] = entityMap.Machine[1].get('PROPS', {}).get('tags', {}).get('vcs-account-id')
+            return alert
+
+        return alert
 
     def get_alert(alert_id, api_key, api_secret, account, subaccount=None):
         logging.info('{0} tenant alert {1} received'.format(account, alert_id))
@@ -18,7 +58,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             account=account,
             subaccount=subaccount
         )
-        
+
         alert = lacework_client.alerts.get_details(alert_id, "Details").get('data', {})
         alert = dict((k.lower(), v) for k,v in alert.items())
         return alert
@@ -57,6 +97,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(body=json.dumps(req_body), headers=HEADERS)
 
     RESPONSE = get_alert(ALERT_ID, API_KEY, API_SECRET, ACCOUNT, SUBACCOUNT)
+    RESPONSE = get_csp_info(RESPONSE)
     RESPONSE.update( (('lw_account', ACCOUNT), ('alert_url', ALERT_LINK) , ('lw_subaccount', SUBACCOUNT)) )
 
     return func.HttpResponse(body=json.dumps(RESPONSE), headers=HEADERS)
